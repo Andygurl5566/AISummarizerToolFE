808f2d0c148057af059c7f40cc6947cb1abb229b
Afreeman <79459307+Andygurl5566@users.noreply.github.com>
2026-02-05 14:36:54 -0600

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+async function analyzeText() {
+  const text = document.getElementById("input").value;
+
+  const response = await fetch("/analyze", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text }),
+  });
+}
+
+const data = await response.json();
+console.log(data);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <></>;
 }
 
 export default App;
